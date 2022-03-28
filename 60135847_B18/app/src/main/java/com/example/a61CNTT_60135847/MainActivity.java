608ce11f6cf65074a18c7b142544937ab9039e32
trương nguyễn đình khoa 60135847 <63198645+truognguyendinhkhoa@users.noreply.github.com>
2022-03-28 17:11:10 +0700

    Tuan4

---
+++ 60135847_B18/app/src/main/java/com/example/a61CNTT_60135847/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a61CNTT_60135847;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity  {
+    TextView selection;
+    String arr[]={"Intel","SamSung",
+            "Nokia","Simen","AMD",
+            "KIC","ECD"};
+    ArrayAdapter<String >adapter=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter=new ArrayAdapter<String>
+                (this,
+                        android.R.layout.simple_list_item_1,
+                        arr);
+        setListAdapter(adapter);
+
+        selection=(TextView) findViewById(R.id.selection);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String txt="postion = "+position +"; value ="+arr[position];
+        selection.setText(txt);
+    }
+}
